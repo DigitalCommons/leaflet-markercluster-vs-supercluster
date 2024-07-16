dd4f56d99008ad70064b2e1fa7317513bd6950a0
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2024-07-16 16:53:42 +0100

    superclusterTest.js - reindent

--- superclusterTest.js
+++ superclusterTest.js
@@ -30,9 +30,9 @@ const generateMarkers = (count) => {
   const bounds = new L.latLngBounds(southWest, northEast);
 
   const minLat = bounds.getSouthWest().lat,
-    rangeLng = bounds.getNorthEast().lat - minLat,
-    minLng = bounds.getSouthWest().lng,
-    rangeLat = bounds.getNorthEast().lng - minLng;
+        rangeLng = bounds.getNorthEast().lat - minLat,
+        minLng = bounds.getSouthWest().lng,
+        rangeLat = bounds.getNorthEast().lng - minLng;
 
   const result = Array.from({ length: count }, (v, k) => {
     return {
@@ -100,23 +100,23 @@ const markers = L.geoJson(null, {
 
 // Callback to get the clusters on pan and zoom
 function reCluster(e) {
-    console.log("reclustering", e);
-    performance.mark("supercluster-recluster-start")
-    const zoom = map.getZoom();
-    const bounds = map.getBounds();
-    const bbox = [
-      bounds.getWest(),
-      bounds.getSouth(),
-      bounds.getEast(),
-      bounds.getNorth(),
-    ];
-    
-    const clusters = index.getClusters(bbox, zoom);
-    
-    markers.clearLayers();
-    markers.addData(clusters);
-    performance.measure("supercluster-recluster-complete", "supercluster-recluster-start");
-    
+  console.log("reclustering", e);
+  performance.mark("supercluster-recluster-start")
+  const zoom = map.getZoom();
+  const bounds = map.getBounds();
+  const bbox = [
+    bounds.getWest(),
+    bounds.getSouth(),
+    bounds.getEast(),
+    bounds.getNorth(),
+  ];
+  
+  const clusters = index.getClusters(bbox, zoom);
+  
+  markers.clearLayers();
+  markers.addData(clusters);
+  performance.measure("supercluster-recluster-complete", "supercluster-recluster-start");
+  
 }
 
 map.on('zoomend viewreset load moveend zoomlevelschange ', reCluster);
@@ -124,79 +124,80 @@ map.on('zoomend viewreset load moveend zoomlevelschange ', reCluster);
 // Callback to spiderify a (top-level) cluster
 let spidermarkers = [];
 function spider(e) {
-    console.log("spider", e, this);
-    unspider();
-    if (this.feature.properties.cluster) {
-        const children = index
-              .getChildren(this.feature.properties.cluster_id)
-              .filter(f => !f.properties.cluster);
-        console.log("children", children);
-        const center = map.latLngToLayerPoint(this.latlng);
-        const points = generatePointsCircle(children.length, center);
-        points.forEach((point, ix) => {
-            const newPos = map.layerPointToLatLng(point);
-            const m = createClusterIcon(children[ix], newPos);
-            if (m.setZIndexOffset) {
-                m.setZIndexOffset(1000000); //Make these appear on top of EVERYTHING
-            }
+  console.log("spider", e, this);
+  unspider();
+  if (this.feature.properties.cluster) {
+    const children = index
+          .getChildren(this.feature.properties.cluster_id)
+          .filter(f => !f.properties.cluster);
+    console.log("children", children);
+    const center = map.latLngToLayerPoint(this.latlng);
+    const points = generatePointsCircle(children.length, center);
+    points.forEach((point, ix) => {
+      const newPos = map.layerPointToLatLng(point);
+      const m = createClusterIcon(children[ix], newPos);
+      if (m.setZIndexOffset) {
+        m.setZIndexOffset(1000000); //Make these appear on top of EVERYTHING
+      }
 
-            spidermarkers.push(m);
-            map.addLayer(m);
-        });
+      spidermarkers.push(m);
+      map.addLayer(m);
+    });
 
-        return;
-        
-        function generatePointsCircle(count, centerPt) {
-            const spiderfyDistanceMultiplier = 3;
-            const circleFootSeparation = 2;
-            const TWOPI = 3.1415926535927 * 2;
-            const circumference = spiderfyDistanceMultiplier * circleFootSeparation * (2 + count);
-            const res = [];
-            const clusterIconRadius = 35; // Minimum distance to get outside the cluster icon.
-            const legLength = clusterIconRadius;
-            
-            res.length = count;
+    return;
+    
+    function generatePointsCircle(count, centerPt) {
+      const spiderfyDistanceMultiplier = 3;
+      const circleFootSeparation = 2;
+      const TWOPI = 3.1415926535927 * 2;
+      const circumference = spiderfyDistanceMultiplier * circleFootSeparation * (2 + count);
+      const res = [];
+      const clusterIconRadius = 35; // Minimum distance to get outside the cluster icon.
+      const legLength = clusterIconRadius;
+      
+      res.length = count;
 
-            const threshold = 11.3; // a deliberately unharmonious number
-            const [spiralStep, angleStep] = count > threshold?
-                  [legLength * 1.5 / threshold, TWOPI / threshold] :
-                  [0, TWOPI / count];
-            
-            for (let i = 0; i < count; i++) { // Clockwise, like spiral.
-                const length = legLength + i*spiralStep;
-                const angle = 0 + i*angleStep;
-                res[i] = new L.Point(centerPt.x + length * Math.cos(angle),
-                                     centerPt.y + length * Math.sin(angle))._round();
-            }
-            
-            return res;
-        };
-    }
+      const threshold = 11.3; // a deliberately unharmonious number
+      const [spiralStep, angleStep] = count > threshold?
+            [legLength * 1.5 / threshold, TWOPI / threshold] :
+            [0, TWOPI / count];
+      
+      for (let i = 0; i < count; i++) { // Clockwise, like spiral.
+        const length = legLength + i*spiralStep;
+        const angle = 0 + i*angleStep;
+        res[i] = new L.Point(centerPt.x + length * Math.cos(angle),
+                             centerPt.y + length * Math.sin(angle))._round();
+      }
+      
+      return res;
+    };
+  }
 }
 
 function unspider(e) {
-    spidermarkers.map(m => map.removeLayer(m));
-    spidermarkers = [];
+  spidermarkers.map(m => map.removeLayer(m));
+  spidermarkers = [];
 }
 
 function createClusterIcon(feature, latlng) {
-    let opts = {}; 
-    if (feature.properties.cluster) {
-        
-        const count = feature.properties.point_count;
-        const size = count < 100 ? "small" : count < 1000 ? "medium" : "large";
-        opts.icon = L.divIcon({
-            html: `<div><span>${feature.properties.point_count_abbreviated}</span></div>`,
-            className: `marker-cluster marker-cluster-${size}`,
-            iconSize: L.point(40, 40),
-        });
-        
-    }
+  let opts = {}; 
+  if (feature.properties.cluster) {
     
+    const count = feature.properties.point_count;
+    const size = count < 100 ? "small" : count < 1000 ? "medium" : "large";
+    opts.icon = L.divIcon({
+      html: `<div><span>${feature.properties.point_count_abbreviated}</span></div>`,
+      className: `marker-cluster marker-cluster-${size}`,
+      iconSize: L.point(40, 40),
+    });
     
-    const marker = L.marker(latlng, opts);
-    marker.on('click', spider, {feature, latlng});
-    return marker;
+  }
+  
+  
+  
+  const marker = L.marker(latlng, opts);
+  marker.on('click', spider, {feature, latlng});
+  return marker;
 }
 
 function mkDirectory(data) {
@@ -244,12 +245,9 @@ fetch(data).then(async (res) => {
   performance.measure("supercluster-load-complete", "supercluster-load-start");
   console.log("got index");
   reCluster();
+  console.log(`cluster created at ${new Date().toUTCString()}`);
 });
 
 panelRight.addTo(map);
-
-
 markers.addTo(map);
-reCluster();
 
-console.log(`cluster created at ${new Date().toUTCString()}`);
