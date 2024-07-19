af63212d0673d6ec9748122ffd575861cb4df714
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2024-07-19 19:17:11 +0100

    gen-data.rb - generate a 1e6 lat/lng file and a 50 lat/lng file

--- gen-data.rb
+++ gen-data.rb
@@ -1,11 +1,35 @@
 #!/bin/env ruby
 
-num = (ARGV[0] || 1).to_i
-puts "Generating #{num} points"
-points = File.open("points.bin", 'w')
+def write_block(out, num, template)
+  str = if out.is_a? String
+          File.open(out, 'w')
+        else
+          out
+        end
+  
+  num.times do |i|
+    str << yield(i).pack(template)
+  end
 
-num.times do |i|
-  points << [rand*180-90, rand*360-180].pack("gg")
+  str.close unless out.is_a? String
 end
 
-points.close
+def write_lat_lng(out, num)
+  write_block(out, num, "gg") do
+    [rand*180-90, rand*360-180]
+  end
+end
+
+
+num = (ARGV[0] || 5e5).to_i
+
+
+points_bin = "points.bin"
+puts "Generating #{num} points to #{points_bin}"
+write_lat_lng(points_bin, num)
+
+init_points_bin = "initpoints.bin"
+num = 50
+puts "Generating #{num} points to #{init_points_bin}"
+write_lat_lng(init_points_bin, num)
+
